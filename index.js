fefe8187895c12761b236ace7d32d433f50c63f1
Bri <bri.krystalle@gmail.com>
2021-04-01 18:43:45 -0400

    psuedocode added

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 //test
+//psuedocode
+//create a databse of three tables
+//db1 = ID + name
+//db2 = ID + title + salary + department ID
+//db3 = ID + first name + last name + manager ID
+
+//functionality
+//add, view and update the databases
+
+//packages to include
+
+//inquirer
+//a database/sql
+//nom package
+
+//console.table
